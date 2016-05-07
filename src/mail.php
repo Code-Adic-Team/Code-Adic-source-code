3d6f7f2b845904877465dba831773c3cd4ba6507
HittmanA <hittmana@comcast.net>
2016-05-06 19:22:50 -0500

    Added files via upload

---
+++ src/mail.php
@@ -0,0 +1,25 @@
+<?php
+
+session_start();
+include_once 'dbconnect.php';
+
+$res=mysql_query("SELECT * FROM users WHERE user_id=".$_SESSION['user']);
+$userRow=mysql_fetch_array($res);
+
+$account = $userRow['username'];
+
+
+if(!isset($_SESSION['user']))
+{
+ $account = $_POST['name'];
+}
+
+
+$to = "hittmana@comcast.net";
+$subject = $_POST['sub'];
+$txt = $_POST['msg'];
+$email = $_POST['email'];
+$headers = "From: Ahitt-Industries || " . $email . " || " . $account;
+
+mail($to,$subject,$txt,$headers);
+?>
